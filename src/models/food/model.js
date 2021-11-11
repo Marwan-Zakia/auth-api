7a7b91d586605e115bf594ed09712015c0bb739d
Marwan Amir zakia <84706279+Marwan-Zakia@users.noreply.github.com>
2021-11-11 09:20:20 +0200

    new

--- src/models/food/model.js
+++ src/models/food/model.js
@@ -2,8 +2,8 @@
 
 const foodModel = (sequelize, DataTypes) => sequelize.define('Food', {
   name: { type: DataTypes.STRING, required: true },
-  calories: { type: DataTypes.NUMBER, required: true },
+  calories: { type: DataTypes.INTEGER, required: true },
   type: { type: DataTypes.ENUM('fruit', 'vegetable', 'protein'), required: true }
 });
-
+//Integer
 module.exports = foodModel;
